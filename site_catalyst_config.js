1f24b52cb4e82ccbde9268c248c3cdfd8aaa4ac2
Prince <princekumar-d@users.noreply.github.com>
2016-05-20 16:54:51 +0100

    Add files via upload

---
+++ site_catalyst_config.js
@@ -0,0 +1,64 @@
+{
+  "options": {
+    "VISITOR_API": {
+      "MARKETING_CLOUD_ORG_ID": "41E27DA552A6473A0A490D4D@AdobeOrg",
+      "NAMESPACE": "bcgs-test",
+      "TRACKING_SERVER": "tuiuk.d1.sc.omtrdc.net"
+    },
+    "APP_MEASUREMENT": {
+      "RSID": "tuiuktrialprod",
+      "TRACKING_SERVER": "tuiuk.d1.sc.omtrdc.net"
+    },
+    "HEARTBEAT": {
+      "DISABLE": true,
+      "TRACKING_SERVER": "tuiuk.d1.sc.omtrdc.net",
+      "JOB_ID": "sc_va",
+      "PUBLISHER": "41E27DA552A6473A0A490D4D@AdobeOrg",
+      "OVP": "Brightcove",
+      "SDK": "1.5.2",
+      "DEBUG_LOGGING": false
+    },
+    "QUALITY": {
+      "AVERAGE_BITRATE": 0,
+      "TIME_TO_START": 0
+    },
+    "CUSTOM_EVENT": {
+      "disable": false,
+      "bc_data_mapping": {
+        "name": "eVar202,prop2",
+        "segment": "eVar203",
+        "contentType": "eVar201",
+        "timePlayed": "event203",
+        "view": "event201",
+        "segmentView": "event202",
+        "complete": "event207",
+        "milestones": {
+          "25": "event204",
+          "50": "event205",
+          "75": "event206"
+        }
+      },
+      "bc_volumechange": {
+        "event": "event208",
+        "evar": "prop1"
+      },
+      "bc_ad_pause": {
+        "event": "event209",
+        "evar": "eVar205"
+      },
+      "bc_fullscreen_enter": {
+        "event": "event212"
+      },
+      "bc_fullscreen_exit": {
+        "event": "event213"
+      },
+      "bc_social_opened": {
+        "event": "event214"
+      },
+      "bc_social_closed": {
+        "event": "event215"
+      }
+    }
+  },
+  "jQuery": "2.1.4"
+}
