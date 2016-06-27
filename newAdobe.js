e91816a8b90cce2a5597d1dfc6a08d999d9d10a8
Prince <princekumar-d@users.noreply.github.com>
2016-06-27 17:13:39 +0100

    Update newAdobe.js

--- newAdobe.js
+++ newAdobe.js
@@ -1,18 +1,18 @@
 
-videojs.plugin('pluginDev', function() {
+videojs.plugin('siteCatalyst', function() {
           var myPlayer = this;
             var isContentLoaded=false;
             var videoDuration;
             var mediaName;
-            var mediaPlayerName = "Playlist Player";
+            var mediaPlayerName = "TUI Brightcove HTML5 Player";
             var currentTime;
             var isPlaying = false;
           
             function ABDMediaOPEN() {
-                console.log("IN ABDMediaOPEN TOP");
+                //console.log("IN ABDMediaOPEN TOP");
                 //Check the metadata is loaded
                 if(isContentLoaded){
-                    console.log("IN ABDMediaOPEN content loaded");
+                    //console.log("IN ABDMediaOPEN content loaded");
                     //Get all required metadata
                     currentTime = myPlayer.currentTime();
                     mediaName = myPlayer.mediainfo.description;
@@ -21,8 +21,8 @@ videojs.plugin('pluginDev', function() {
                     s.Media.open(mediaName,videoDuration,mediaPlayerName);
                     //Check if video is playing
                     if(isPlaying){
-                        console.log("IN ABDMediaOPEN video is playing " + mediaName + " | " + videoDuration);
-                        console.log();
+                        //console.log("IN ABDMediaOPEN video is playing " + mediaName + " | " + videoDuration);
+                        //console.log();
                         //Play Adobe Analytics Media module from beginning.
                         s.Media.play(mediaName,currentTime);
                     }
@@ -37,7 +37,7 @@ videojs.plugin('pluginDev', function() {
             }
           
             myPlayer.on('loadstart',function(){
-                console.log("loadstart - " + myPlayer.mediainfo.description);
+                //console.log("loadstart - " + myPlayer.mediainfo.description);
                 //Check that metadata is loaded
                 if(myPlayer.mediainfo.description){
                     isContentLoaded = true;
@@ -47,7 +47,7 @@ videojs.plugin('pluginDev', function() {
             });
 
             myPlayer.on('play', function(){
-                console.log("Played - " + myPlayer.mediainfo.description);
+                //console.log("Played - " + myPlayer.mediainfo.description);
                 isPlaying = true;
                 //Check if metadata loaded - needed to make sure correct video media module instance is tracked.
                 if(isContentLoaded){
@@ -59,7 +59,7 @@ videojs.plugin('pluginDev', function() {
 
             myPlayer.on('pause',function(){
                 isPlaying = false;
-                console.log("paused - " + myPlayer.mediainfo.description);
+                //console.log("paused - " + myPlayer.mediainfo.description);
                 //Check if metadata loaded - needed to make sure correct video media module instance is tracked.
                 if(isContentLoaded){
                     currentTime = myPlayer.currentTime();
@@ -68,16 +68,16 @@ videojs.plugin('pluginDev', function() {
                 }
             });
 
-            myPlayer.on('progress',function(){
-                console.log("progressed - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('progress',function(){
+                //console.log("progressed - " + myPlayer.mediainfo.description);
+            //});
 
-            myPlayer.on('resize',function(){
-                console.log("resized - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('resize',function(){
+                //console.log("resized - " + myPlayer.mediainfo.description);
+            //});
 
             myPlayer.on('seeked',function(){
-                console.log("seeked - " + myPlayer.mediainfo.description);
+                //console.log("seeked - " + myPlayer.mediainfo.description);
                 //Check if metadata loaded - needed to make sure correct video media module instance is tracked.
                 if(isContentLoaded){
                     currentTime = myPlayer.currentTime();
@@ -87,7 +87,7 @@ videojs.plugin('pluginDev', function() {
             });
 
             myPlayer.on('seeking',function(){
-                console.log("seeking - " + myPlayer.mediainfo.description);
+                //console.log("seeking - " + myPlayer.mediainfo.description);
                 //Check if metadata loaded - needed to make sure correct video media module instance is tracked.
                 if(isContentLoaded){
                     currentTime = myPlayer.currentTime();
@@ -96,24 +96,24 @@ videojs.plugin('pluginDev', function() {
                 }
             });
 
-            myPlayer.on('timeupdate',function(){
-                console.log("timeupdate - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('timeupdate',function(){
+                //console.log("timeupdate - " + myPlayer.mediainfo.description);
+            //});
 
-            myPlayer.on('volumechange',function(){
-                console.log("volumechange - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('volumechange',function(){
+                //console.log("volumechange - " + myPlayer.mediainfo.description);
+            //});
 
-            myPlayer.on('waiting',function(){
-                console.log("waiting - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('waiting',function(){
+                //console.log("waiting - " + myPlayer.mediainfo.description);
+            //});
 
-            myPlayer.on('durationchange',function(){
-                console.log("durationchange - " + myPlayer.mediainfo.description);
-            });
+           // myPlayer.on('durationchange',function(){
+                //console.log("durationchange - " + myPlayer.mediainfo.description);
+            //});
 
             myPlayer.on('ended',function(){
-                console.log("ended - " + myPlayer.mediainfo.description);
+                //console.log("ended - " + myPlayer.mediainfo.description);
                 //Check if metadata loaded - needed to make sure correct video media module instance is tracked.
                 if(isContentLoaded){
                     currentTime = myPlayer.currentTime();
@@ -124,19 +124,19 @@ videojs.plugin('pluginDev', function() {
                 }
             });
           
-            myPlayer.on('fullscreenchange',function(){
-                console.log("fullscreenchange - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('fullscreenchange',function(){
+                //console.log("fullscreenchange - " + myPlayer.mediainfo.description);
+            //});
 
-            myPlayer.on('loadedalldata',function(){
-                console.log("loadedalldata - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('loadedalldata',function(){
+                //console.log("loadedalldata - " + myPlayer.mediainfo.description);
+            //});
 
-            myPlayer.on('loadeddata',function(){
-                console.log("loadeddata - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('loadeddata',function(){
+                //console.log("loadeddata - " + myPlayer.mediainfo.description);
+            //});
           
-            myPlayer.on('loadedmetadata',function(){
-                console.log("loadedmetadata - " + myPlayer.mediainfo.description);
-            });
+            //myPlayer.on('loadedmetadata',function(){
+                //console.log("loadedmetadata - " + myPlayer.mediainfo.description);
+            //});
 });
